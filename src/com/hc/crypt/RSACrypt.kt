af7b041c7f1247b8e73da719e99943875db539cb
hcc007 <hcwang1024@163.com>
2020-07-09 22:26:16 +0800

    format

--- src/com/hc/crypt/RSACrypt.kt
+++ src/com/hc/crypt/RSACrypt.kt
@@ -8,14 +8,14 @@ import java.security.PublicKey
 import java.util.*
 import javax.crypto.Cipher
 
-object RSACrypt{
+object RSACrypt {
 
     val transformation = "RSA"
     val encrypt_max_size = 117   //加密是 117
     val DECRYPT_MAX_SIZE = 128   //解密是 128
 
 
-    fun  encryptByPrivateKey(input:String,privateKey:PrivateKey): String {
+    fun encryptByPrivateKey(input: String, privateKey: PrivateKey): String {
         //密钥对生成器
 //        val keyPairGenerator = KeyPairGenerator.getInstance("RSA");
 //        //生成密钥对
@@ -23,39 +23,32 @@ object RSACrypt{
 //        val publicKey = keyPair.public
 //        val privateKey = keyPair.private
 
-
-
         val cipher = Cipher.getInstance(transformation)
-        cipher.init(Cipher.ENCRYPT_MODE,privateKey)
+        cipher.init(Cipher.ENCRYPT_MODE, privateKey)
 
         //分段加密
         val byteArray = input.toByteArray()
-        var temp:ByteArray? = null  //缓存
+        var temp: ByteArray? = null  //缓存
         var offset = 0  //当前偏移
 
         val bos = ByteArrayOutputStream()
 
-        while (byteArray.size - offset > 0){
+        while (byteArray.size - offset > 0) {
             //判断是否是最后一块
-            if (byteArray.size - offset >=encrypt_max_size ){
-                 temp =  cipher.doFinal(byteArray,offset, encrypt_max_size)
-                  offset += encrypt_max_size
-            }else{
-                temp = cipher.doFinal(byteArray,offset,byteArray.size - offset)
+            if (byteArray.size - offset >= encrypt_max_size) {
+                temp = cipher.doFinal(byteArray, offset, encrypt_max_size)
+                offset += encrypt_max_size
+            } else {
+                temp = cipher.doFinal(byteArray, offset, byteArray.size - offset)
                 offset += byteArray.size
             }
             bos.write(temp)
         }
-
         bos.close()
-
         return Base64Util.encode(bos.toByteArray())
     }
 
-
-
-
-    fun  encryptByPublicKey(input:String,publicKey: PublicKey): String {
+    fun encryptByPublicKey(input: String, publicKey: PublicKey): String {
 //        //密钥对生成器
 //        val keyPairGenerator = KeyPairGenerator.getInstance("RSA");
 //        //生成密钥对
@@ -74,22 +67,22 @@ object RSACrypt{
 
 
         val cipher = Cipher.getInstance(transformation)
-        cipher.init(Cipher.ENCRYPT_MODE,publicKey)
+        cipher.init(Cipher.ENCRYPT_MODE, publicKey)
 
         //分段加密
         val byteArray = input.toByteArray()
-        var temp:ByteArray? = null  //缓存
+        var temp: ByteArray? = null  //缓存
         var offset = 0  //当前偏移
 
         val bos = ByteArrayOutputStream()
 
-        while (byteArray.size - offset > 0){
+        while (byteArray.size - offset > 0) {
             //判断是否是最后一块
-            if (byteArray.size - offset >=encrypt_max_size ){
-                temp =  cipher.doFinal(byteArray,offset, encrypt_max_size)
+            if (byteArray.size - offset >= encrypt_max_size) {
+                temp = cipher.doFinal(byteArray, offset, encrypt_max_size)
                 offset += encrypt_max_size
-            }else{
-                temp = cipher.doFinal(byteArray,offset,byteArray.size - offset)
+            } else {
+                temp = cipher.doFinal(byteArray, offset, byteArray.size - offset)
                 offset = byteArray.size
             }
             bos.write(temp)
@@ -98,15 +91,13 @@ object RSACrypt{
         bos.close()
 
         return Base64Util.encode(bos.toByteArray())
-
-
     }
 
 
     /**
      * 私钥解密
      */
-    fun  decryptByPrivateKey(input:String,privateKey:PrivateKey): String {
+    fun decryptByPrivateKey(input: String, privateKey: PrivateKey): String {
         //密钥对生成器
 //        val keyPairGenerator = KeyPairGenerator.getInstance("RSA");
 //        //生成密钥对
@@ -115,20 +106,20 @@ object RSACrypt{
 //        val privateKey = keyPair.private
 
         //先解码
-       // val byteArray = Base64Util.decode(input)
+        // val byteArray = Base64Util.decode(input)
         val byteArray = Base64.getDecoder().decode(input)
         val cipher = Cipher.getInstance("RSA")
-        cipher.init(Cipher.DECRYPT_MODE,privateKey)
-        var temp:ByteArray  //缓存
+        cipher.init(Cipher.DECRYPT_MODE, privateKey)
+        var temp: ByteArray  //缓存
         var offset = 0  //当前偏移
         val bos = ByteArrayOutputStream()
-        while (byteArray.size - offset > 0){
+        while (byteArray.size - offset > 0) {
             //判断是否是最后一块
-            if (byteArray.size - offset >=DECRYPT_MAX_SIZE ){
-                temp =  cipher.doFinal(byteArray,offset, DECRYPT_MAX_SIZE)
+            if (byteArray.size - offset >= DECRYPT_MAX_SIZE) {
+                temp = cipher.doFinal(byteArray, offset, DECRYPT_MAX_SIZE)
                 offset += DECRYPT_MAX_SIZE
-            }else{
-                temp = cipher.doFinal(byteArray,offset,byteArray.size - offset)
+            } else {
+                temp = cipher.doFinal(byteArray, offset, byteArray.size - offset)
                 offset = byteArray.size
             }
             bos.write(temp)
@@ -158,9 +149,7 @@ object RSACrypt{
         return String(bos.toByteArray())
     }
 
-
-
-    fun  decryptByPublicKey(input:String,publicKey: PublicKey): String {
+    fun decryptByPublicKey(input: String, publicKey: PublicKey): String {
 //        //密钥对生成器
 //        val keyPairGenerator = KeyPairGenerator.getInstance("RSA");
 //        //生成密钥对
@@ -179,45 +168,37 @@ object RSACrypt{
 
 
         val cipher = Cipher.getInstance(transformation)
-        cipher.init(Cipher.DECRYPT_MODE,publicKey)
+        cipher.init(Cipher.DECRYPT_MODE, publicKey)
 
         //分段加密
         val byteArray = Base64Util.decode(input)
 
 
-        var temp:ByteArray? = null  //缓存
+        var temp: ByteArray? = null  //缓存
         var offset = 0  //当前偏移
 
         val bos = ByteArrayOutputStream()
 
-        while (byteArray.size - offset > 0){
+        while (byteArray.size - offset > 0) {
             //判断是否是最后一块
-            if (byteArray.size - offset >=DECRYPT_MAX_SIZE ){
-                temp =  cipher.doFinal(byteArray,offset, DECRYPT_MAX_SIZE)
+            if (byteArray.size - offset >= DECRYPT_MAX_SIZE) {
+                temp = cipher.doFinal(byteArray, offset, DECRYPT_MAX_SIZE)
                 offset += DECRYPT_MAX_SIZE
-            }else{
-                temp = cipher.doFinal(byteArray,offset,byteArray.size - offset)
+            } else {
+                temp = cipher.doFinal(byteArray, offset, byteArray.size - offset)
                 offset = byteArray.size
             }
             bos.write(temp)
         }
 
         bos.close()
-
         return String(bos.toByteArray())
-
-
     }
-
-
-
-
 
 }
 
-
 //rsa 非对称加密
-fun  main(args:Array<String>){
+fun main(args: Array<String>) {
     //1、创建 cipher
     //2、初始化cipher
     //3、加密/解密
@@ -226,7 +207,7 @@ fun  main(args:Array<String>){
     //密钥对生成器
     val keyPairGenerator = KeyPairGenerator.getInstance("RSA");
     //生成密钥对
-    val keyPair  = keyPairGenerator.genKeyPair();
+    val keyPair = keyPairGenerator.genKeyPair();
     val publicKey = keyPair.public
     val privateKey = keyPair.private
 
@@ -234,10 +215,10 @@ fun  main(args:Array<String>){
     println("private:" + Base64Util.encode(privateKey.encoded))
 
     val input = "这是 rsa 非对称加密原文，这是长度非常长，测试分段加密，这是 rsa 非对称加密原文，这是长度非常长，测试分段加密，这是 rsa 非对称加密原文，这是长度非常长，测试分段加密，这是 rsa 非对称加密原文，这是长度非常长，测试分段加密，"
-    val encrypt = RSACrypt.encryptByPublicKey(input,publicKey)
+    val encrypt = RSACrypt.encryptByPublicKey(input, publicKey)
     println("加密之后:" + encrypt)
 
-    val decrypt = RSACrypt.decryptByPrivateKey(encrypt,privateKey)
+    val decrypt = RSACrypt.decryptByPrivateKey(encrypt, privateKey)
     println("私钥解密：" + decrypt)
 
 }
